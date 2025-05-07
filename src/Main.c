ffb17aae35ca305d502a8b5593766e9dd5130a93
rickojn <rickojn@hotmail.com>
2025-05-07 22:02:53 +0100

    reorg repo

--- src/Main.c
+++ src/Main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include  "CustardFlow.h"
 
-#define M 10000
+#define M 12000
 #define N 256
 #define K 784
 #define TILE 128
@@ -12,161 +13,6 @@
 #define TILED 0
 #define L1 1
 #define ONLY_LARGE 1
-
-
-
-size_t min(size_t a, size_t b){
-    return a < b ? a : b;
-}
-
-void naive_matmul(const float* A, const float *B, float * C, size_t rows_C, size_t cols_C, size_t rows_B_cols_A){
-    for (size_t i = 0; i < rows_C; i++ ){
-        for (size_t j = 0; j < cols_C; j++){
-            for (size_t k = 0; k < rows_B_cols_A; k++){            
-                C[i * cols_C + j] += A[i * rows_B_cols_A + k] * B[j * cols_C + k]; 
-            }
-        }
-    }
-}
-
-
-
-
-
-
-
-/*
-
-m = 3 n= 4 k = 2
-
-1 10    1 1 1 1
-2 20    2 2 2 2
-3 30    
-
-21 21 21 21
-42 42 42 42
-63 63 63 63
-
-sum of outer products
-
-first column by first row:
-1  (x)   1 1 1 1
-2
-3
-
-=
-
-1 1 1 1
-2 2 2 2
-3 3 3 3
-
-
-
-second column by second row:
-
-10  (x)   2 2 2 2
-20
-30
-
-20 20 20 20
-40 40 40 40
-60 60 60 60
- 
-
-
-sum of outer products:
-
-1 1 1 1   20 20 20 20
-2 2 2 2 + 40 40 40 40
-3 3 3 3   60 60 60 60
-=
-21 21 21 21
-42 42 42 42
-63 63 63 63
-
-
-*/
-
-void outer_product_matmul(const float * a, const float * b, float * c, size_t m, size_t n, size_t k ){
-    for (size_t idx_k = 0; idx_k < k; idx_k++){
-        for (size_t idx_m = 0; idx_m < m; idx_m++){
-            for (size_t idx_n = 0; idx_n < n; idx_n++){
-                // size_t offset_a = idx_k * m  + idx_m; // a[m][k] col major 
-                // size_t offset_b = idx_k * n + idx_n;  // b[k][n]  row major
-                // size_t offset_c = idx_m * n + idx_n; // row major
-                // c[offset_c] += a[offset_a] * b[offset_b];
-                c[idx_m * n +idx_n] += a[idx_k * m +idx_m] * b[idx_k * n + idx_n];
-            }
-        }
-    }
-}
-
-void tiled_matmul(const float * A, const float * B, float * C, size_t m, size_t n, size_t k, size_t size_tile){
-    for (size_t tile_start_m = 0; tile_start_m < m; tile_start_m += size_tile){
-        for (size_t tile_start_n = 0; tile_start_n < n; tile_start_n += size_tile){
-            for (size_t tile_start_k = 0; tile_start_k < k; tile_start_k += size_tile){
-
-
-                for (size_t idx_m = tile_start_m; idx_m < tile_start_m + size_tile && idx_m < m; idx_m++){
-                    for (size_t idx_n = tile_start_n; idx_n < tile_start_n + size_tile && idx_n < n; idx_n++){
-                        float sum = 0.0;
-                        // each k (col of A and row of B)
-                        size_t offset_a = idx_m * k;
-                        size_t offset_b = idx_n * n;
-                        for (size_t idx_k = tile_start_k; idx_k < tile_start_k + size_tile && idx_k < k; idx_k++){
-                            sum += A[offset_a + idx_k] * B[idx_k + offset_b];
-                            }
-                        C[idx_m * n + idx_n] += sum;
-                    }
-                }
-
-
-            }
-        }
-    }
-}
-
-
-
-void l1_tiled_matmul(const float * A, const float * B, float * C, size_t m, size_t n, size_t k, size_t size_outer_tile, size_t size_inner_tile){
-    for (size_t idx_m = 0; idx_m < m; idx_m += size_outer_tile){
-        for (size_t idx_n = 0; idx_n < n; idx_n += size_outer_tile){
-            for (size_t idx_k = 0; idx_k < k; idx_k += size_outer_tile){
-                
-                for (size_t idx_mm = idx_m; idx_mm < idx_m + size_outer_tile && idx_mm < m; idx_mm += size_inner_tile){
-                    for (size_t idx_nn = idx_n; idx_nn < idx_n + size_outer_tile && idx_nn < n; idx_nn += size_inner_tile){
-                        for (size_t idx_kk = idx_k; idx_kk < idx_k + size_outer_tile && idx_kk < k; idx_kk += size_inner_tile){
-
-                            for (size_t idx_mmm = idx_mm; idx_mmm < idx_mm + size_inner_tile && idx_mmm < m; idx_mmm++){
-                                for (size_t idx_nnn = idx_nn; idx_nnn < idx_nn + size_inner_tile && idx_nnn < n; idx_nnn++){
-                                    float sum = 0;
-                                    size_t offset_a = idx_mmm * k; // A[idx_mmm][0] row major
-                                    size_t offset_b = idx_nnn * k; // B[0][idx_nnn] col major
-                                    size_t idx_kkk = idx_kk;
-                                    for (;idx_kkk < idx_kk + size_inner_tile && idx_kkk < k; idx_kkk+= 8){
-                                        sum += A[offset_a + idx_kkk] * B[offset_b + idx_kkk];
-                                        sum += A[offset_a + idx_kkk + 1] * B[offset_b + idx_kkk + 1];
-                                        sum += A[offset_a + idx_kkk + 2] * B[offset_b + idx_kkk + 2];
-                                        sum += A[offset_a + idx_kkk + 3] * B[offset_b + idx_kkk + 3];
-                                        sum += A[offset_a + idx_kkk + 4] * B[offset_b + idx_kkk + 4];
-                                        sum += A[offset_a + idx_kkk + 5] * B[offset_b + idx_kkk + 5];
-                                        sum += A[offset_a + idx_kkk + 6] * B[offset_b + idx_kkk + 6];
-                                        sum += A[offset_a + idx_kkk + 7] * B[offset_b + idx_kkk + 7];
-                                    }
-                                    for (; idx_kkk < idx_kk + size_inner_tile && idx_kkk < k; idx_kkk++){
-                                        sum += A[offset_a + idx_kkk] * B[offset_b +idx_kkk];
-                                    }
-                                    C[idx_mmm * n + idx_nnn] += sum;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
 
 
 
@@ -193,6 +39,7 @@ void check_result(const float * ref_result, const float * result, size_t m, size
         }
     }
 }
+
 
 
 
